32f134edc33ee4196d699db9651cc1f509310fb6
BV Satyaram <bvsatyaram@gmail.com>
2016-08-03 16:20:33 +0530

    Introduce RSpec for testing

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  it "validates presence of title" do
+    task = Task.new
+    expect(task.title).to be_nil
+    expect(task).not_to be_valid
+
+    task.title = 'Eat'
+    expect(task).to be_valid
+  end
+end
